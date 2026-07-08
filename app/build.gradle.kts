77fe57c0a4fb265f2180644c52fda7592b7dfeff
siamirc <lemon@thaiirc.com>
2026-07-08 20:56:00 +0700

    chore: bump version to 1.2 and remove CI workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.thaiirc.chatradio.wvmzpl"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.1"
+    versionCode = 3
+    versionName = "1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
